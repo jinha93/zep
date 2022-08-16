a248da867a806a5c9ab126cb27f2dbc5ced44498
ParkJinha <jhhhhh93@naver.com>
2022-08-16 17:00:20 +0900

    '미니맵수정'

--- 미니맵/main.js
+++ 미니맵/main.js
@@ -1,9 +1,11 @@
 //UI가 노출될 위치(정렬, 가로, 세로) 값을 변수로 사전에 제작
-let position = 'middle';
-let width = 350;
-let height = 350;
-
+let _position = 'top-left';
+let _width = Map.width * 5;
+let _height = Map.height * 5;
 let _players = App.players;
+
+// 위젯에 보낼 맵 이미지 로드
+let _mapImg = App.loadSpritesheet("map.png");
 
 // 플레이어가 입장 할 때 동작하는 함수
 App.onJoinPlayer.Add(function (p) {
@@ -13,7 +15,14 @@ App.onJoinPlayer.Add(function (p) {
 		widget: null,
 	};
 	
-	p.tag.widget = p.showWidget('my.html', position, width, height);
+	p.tag.widget = p.showWidget('my.html', _position, _width+60, _height+20);
+	p.tag.widget.sendMessage({
+		mapImg: _mapImg.body, //map.body는 map 이미지의 base64 data를 가르킴
+		width: _width,
+		height: _height,
+		tileX: p.tileX,
+		tileY: p.tileY,
+	});
 });
 
 // 플레이어가 퇴장 할 때 동작하는 함수
@@ -39,12 +48,12 @@ App.onUpdate.Add(function(dt) {
 		}
 		
 		p.tag.widget.sendMessage({
+			width: _width,
+			height: _height,
 			tileX: p.tileX,
 			tileY: p.tileY,
 		});
 		
 	}
 });
-
-
 
